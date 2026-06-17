1964363acaf2dc69937b60a4e5e3a890766f448e
coolmekjidfur <belaebelwood2005@plovmail.com>
2026-06-17 02:43:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const BOT_TOKEN = '8758420434:AAE0OU24YuMfEShOxDkQ7kkE-y6RqzbrysM';
+const BOT_TOKEN = '8747201279:AAF6oGajNQZL2OZ-6Iq2ak5FpF3mRQ2lE9c';
 const BACKEND_URL = 'https://effective-spork-lixa.onrender.com';
 
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
